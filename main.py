82f1e870c28736b2884b768d3169ff3d3c2f5c08
lplping <506360587@qq.com>
2020-07-01 15:48:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import modeling
 from utils import *
 from model_s_po import model_fn
 os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-os.environ['CUDA__VISIBLE_DEVICES'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 flags = tf.flags
 
